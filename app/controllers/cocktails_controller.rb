e83ed770091da3ff1a92c91d754f3dc0199e276a
Lucas Koerner <lcskoerner@gmail.com>
2020-08-15 17:35:15 -0400

    controllers actions

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,30 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def create
+    @cocktail = Cocktail.new(cocktails_params)
+    @cocktail.save
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,31 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.destroy(params[:id])
+    @dose.destroy
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
