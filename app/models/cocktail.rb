5799bcba4c7d8c3f67f4c6bac03955ac892a9827
Lucas Koerner <lcskoerner@gmail.com>
2020-08-18 21:06:19 -0400

    removed select2 from view and validate photo mandatory

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
+  validates :photo, presence: true
 end
