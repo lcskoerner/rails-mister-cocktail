e0711ff074c4a72bad7407e37131262e09c53c56
Lucas Koerner <lcskoerner@gmail.com>
2020-08-21 00:22:57 -0400

    image size adjust

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
-  validates :photo, presence: true, dimension: { min: 1100..1600 }, size: { less_than: 2.megabytes, message: 'max size is 2 mb' }
+  validates :photo, presence: true, dimension: { min: 440..500 }, size: { less_than: 2.megabytes, message: 'max size is 2 mb' }
 end
